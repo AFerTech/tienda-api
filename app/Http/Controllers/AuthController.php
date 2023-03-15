3908d8c9d625bf32582bdad0aebc760fc07ba190
Alexis Fernando <alexis96fer@gmail.com>
2023-03-15 01:10:26 -0600

    Add reglas de validación

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\RegistroRequest;
 
 class AuthController extends Controller
 {
 
-    public function register(Request $request){
+    public function register(RegistroRequest $request){
+        // validar registro
+        $data = $request->validated();
 
     }
-    public function login(Request $request){
+    public function login(RegistroRequest $request){
 
     }
-    public function logout(Request $request){
+    public function logout(RegistroRequest $request){
 
     }
 }
